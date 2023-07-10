a366772f633ab3ef41ca95609896b4fe54ced414
deskimir <desimirdimovic@gmail.com>
2023-07-10 12:37:07 +0200

    camera fixed

--- test/Camera2D.gd
+++ test/Camera2D.gd
@@ -2,12 +2,12 @@ extends Camera2D
 
 @onready var topLeft = $Limits/TopLeft
 @onready var bottomRight = $Limits/BottomRight
+@onready var area_2d = $Area2D
 
 
 func _ready():
 	limit_top = topLeft.position.y
-	limit_left = topLeft.position.x
 	limit_bottom = bottomRight.position.y
-	limit_right = bottomRight.position.x
 
-
+func _on_area_2d_area_entered(area):
+	limit_left = area.global_position.x
--- world.gd
+++ world.gd
@@ -15,11 +15,10 @@ extends Node2D
 @onready var win = $Camera2D/CanvasLayer2/Win
 
 
-var limits = [240, 600, 1200, 1200,1800,1800, 2500, 2500, 2500, 2500, 25000,25000]
-
 var enemies_spawned = true
 var player_x_max = 120
 var triger_counter = 0
+
 
 func _ready():
 	game_over.visible = false
@@ -31,31 +30,18 @@ func _ready():
 
 
 func _process(delta):
-	camera_2d.limit_right = limits[triger_counter]
-	block_front.global_position.x = camera_2d.limit_right + 20
-	if Global.player_pos.x > player_x_max:
-		if abs(camera_2d.limit_right - Global.player_pos.x) > 119:
-			player_x_max = Global.player_pos.x
-			camera_2d.limit_left = player_x_max - 120
-			block_behind.global_position.x = camera_2d.limit_left
-	#fix camera limits when entering areas of right but when level is all done should be easy to hard code it in design
-	if Input.is_action_just_pressed("cam_test_down"):
-		camera_2d.limit_right += 240
+	block_behind.global_position.x = camera_2d.limit_left
+	
 	if enemies.get_child_count() == 0 and enemies_spawned:
-		#spawner_count += 1
 		if triger_counter == 8:
 			win.visible = true
 			win.play_anim()
 		go_popup_timer.start()
 		go_popup.visible = true
-		#camera_2d.limit_right += 240
-		#camera_2d.limit_right = spawner_count * 600
 		enemies_spawned = false
-
 
 func instance_node(node, location):
 	call_deferred("_instance_node_call", node, location)
-	
 
 func _instance_node_call(node, location):
 	var node_instance = node.instantiate()
@@ -66,12 +52,9 @@ func on_player_no_health():
 	game_over.visible = true
 	game_over.play_anim()
 	Global.paused = true
-	
-
 
 func _on_go_popup_timer_timeout():
 	go_popup.visible = false
-	#SoundPlayer.stop_all()
 
 func _on_triger_entered(position_x, num, type):
 	triger_counter += 1
@@ -80,8 +63,6 @@ func _on_triger_entered(position_x, num, type):
 	
 
 func _spawn_enemies(position_x, num, type):
-	#spawner_count += 1
-	#camera_2d.limit_right = spawner_count * 600
 	if type == 1:
 		for i in range(num):
 			var node_insance = enemy.instantiate()
